c8dbad4bd5f94cb54b6bcf929946424a7508b9ad
Landon Bass <landon@labtechnologygroup.com>
2016-05-04 17:38:58 -0400

    continued test setup saga

---
+++ test/utilityTest.js
@@ -0,0 +1,12 @@
+"use strict";
+/*
+
+
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      Chai.assert.equal(-1, [1,2,3].indexOf(5));
+      Chai.assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});*/ 
---
+++ test/utilityTest.ts
@@ -0,0 +1,20 @@
+"use strict";
+
+/// <reference path="../typings/chai/chai.d.ts" />
+
+
+
+
+import Chai = require("chai");
+import * as Utility from "../src/utility";
+/*
+
+
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      Chai.assert.equal(-1, [1,2,3].indexOf(5));
+      Chai.assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});*/
