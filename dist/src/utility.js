5390cff1f69002de89b920cad82b0c08e26e6e22
Landon Bass <landon@labtechnologygroup.com>
2016-05-19 08:02:05 -0400

    comment cleanup

--- dist/src/utility.js
+++ dist/src/utility.js
@@ -1,5 +1,7 @@
 "use strict";
+// no op function used as placeholders where void are returned. TODO research a refactor
 exports.noop = () => { };
+// simple test to see if a value is not null, undefined, or empty string
 exports.isNullUndefinedEmpty = (value) => {
     if (value === null || value === undefined || value === "")
         return true;
--- src/utility.ts
+++ src/utility.ts
@@ -1,7 +1,9 @@
 "use strict";
 
+// no op function used as placeholders where void are returned. TODO research a refactor
 export const noop = () => {};
 
+// simple test to see if a value is not null, undefined, or empty string
 export const isNullUndefinedEmpty = (value: any) : boolean => {
     if (value === null || value === undefined || value === "") return true;
     return false;
